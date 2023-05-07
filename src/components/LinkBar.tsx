b8771959da6e38d84077816450ebdc6eab72835b
davidly <david26ly@gmail.com>
2023-05-06 21:36:40 -0400

    removed unused import

--- src/components/LinkBar.tsx
+++ src/components/LinkBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Resume from "../assets/Resume.pdf";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
